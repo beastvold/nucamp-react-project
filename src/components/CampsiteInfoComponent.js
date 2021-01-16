fdd13fee40b4d1851d862b96775d076f39b11ab8
Ben Eastvold <beastvold@gmail.com>
2021-01-16 06:48:34 -0800

    Added POST comments using Fetch API.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
     
     handleSubmit = (values) => {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -116,7 +116,7 @@ function RenderCampsite({campsite}) {
 //  card. Otherwise, they will stack below.
 // Notice how we are able to use "map" to iterate through and display each
 //  comment in its own div.
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return(
             <div className="col-md-5 m-1">
@@ -130,7 +130,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     )
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -175,11 +175,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                    />                 
                     
                 </div>
             </div>
